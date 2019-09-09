02066325afa105767b128d56d2433f1579107438
Daniel Rodriguez <daniel_a_rodriguez@outlook.com>
2019-09-08 18:15:19 -0700

    chapter 2 try it yourself programs

---
+++ chapter_02/famous_quote.py
@@ -0,0 +1,7 @@
+# --------------------------------------------------------------------------- #
+# D. Rodriguez, 2019-09-08
+# --------------------------------------------------------------------------- #
+quote = 'No es lo mismo acero inoxidable que hacerlo inolvidable'
+name = 'Silvestre Dangond'
+message = f'{name} dijo una vez "{quote}"'
+print(message)
