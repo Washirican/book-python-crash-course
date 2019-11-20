0eaab136535b1d40dadbfd3c52b6c66992fb1103
Washirican <danrod@uw.edu>
2019-11-19 22:40:10 -0800

    Try it yourself files

---
+++ chapter_03/tiy_3_4_guest_list.py
@@ -0,0 +1,11 @@
+# --------------------------------------------------------------------------- #
+# Script Title: Guest List
+# Change Log: (Who, When, What)
+# D. Rodriguez, 2019-11-03, Initial release
+# --------------------------------------------------------------------------- #
+
+guests = ['phoebe bridgers', 'manu ginobili', 'kobe bryant', 'neil degrasse tyson']
+
+for name in guests:
+    print(f'{name.title()}, would you like to join me for dinner?')
+    
