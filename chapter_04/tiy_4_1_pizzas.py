3ecf37301fc8b4bbc4a01e108ec79b3885e4f0a6
Daniel Rodriguez <daniel_a_rodriguez@outlook.com>
2019-11-30 20:26:27 -0800

    Chapter 4 Try It Yourself exercises

---
+++ chapter_04/tiy_4_1_pizzas.py
@@ -0,0 +1,9 @@
+# --------------------------------------------------------------------------- #
+# D. Rodriguez, 2019-11-30
+# --------------------------------------------------------------------------- #
+pizzas = ['hawaiian', 'pepperoni', 'Margherita']
+
+for pizza in pizzas:
+    print(f'I like {pizza.title()} pizza.')
+
+print('I really love pizza!')
---
+++ chapter_04/tiy_4_2_animals.py
@@ -0,0 +1,9 @@
+# --------------------------------------------------------------------------- #
+# D. Rodriguez, 2019-11-30
+# --------------------------------------------------------------------------- #
+
+animals = ['tiger', 'lion', 'cheetah']
+for animal in animals:
+    print(f'A {animal.title()} would not make a great pet.')
+
+print('\nAny of these animals would eat you!')
