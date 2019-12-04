f1eb12d983dd83a14ea6271553ed0f84aafb506f
Daniel Rodriguez <daniel_a_rodriguez@outlook.com>
2019-12-04 12:03:12 -0800

    Chapter 4 Try It Yourself exercises

---
+++ chapter_04/tiy_4_13_buffet.py
@@ -0,0 +1,17 @@
+# --------------------------------------------------------------------------- #
+# D. Rodriguez, 2019-12-03
+# --------------------------------------------------------------------------- #
+foods = ('chicken', 'steak', 'fish', 'tofu', 'lamb')
+
+
+print('Original menu:')
+
+for food in foods:
+    print(food.title())
+
+
+foods = ('chicken', 'beans', 'fruit', 'tofu', 'lamb')
+print('\nNew menu:')
+
+for food in foods:
+    print(food.title())
