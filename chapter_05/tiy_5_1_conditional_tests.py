859eb5ca4fba9969a069a3712829f2095cb38e66
Daniel Rodriguez <daniel_a_rodriguez@outlook.com>
2020-01-13 21:26:09 -0800

    Chapter 5 Try It Yourself exercises

---
+++ chapter_05/tiy_5_1_conditional_tests.py
@@ -0,0 +1,18 @@
+# --------------------------------------------------------------------------- #
+# D. Rodriguez, 2020-01-13
+# --------------------------------------------------------------------------- #
+car = 'subaru'
+print('Is car == "subaru"? I predict "True".')
+print(car == 'subaru')
+print()
+
+print('Is car == "honda"? I predict "False".')
+print(car == 'honda')
+print()
+
+print('Is car != "honda"? I predict "True".')
+print(car != 'honda')
+print()
+
+print('Is car != "subaru"? I predict "False".')
+print(car != 'subaru')
---
+++ chapter_05/tiy_5_2_more_conditinal_tests.py
@@ -0,0 +1,11 @@
+# --------------------------------------------------------------------------- #
+# D. Rodriguez, 2020-01-13
+# --------------------------------------------------------------------------- #
+car = 'subaru'
+print('Is car == "subaru"? I predict "True".')
+print(car == 'subaru')
+print()
+
+print('Is car != "honda"? I predict "True".')
+print(car != 'honda')
+print()
