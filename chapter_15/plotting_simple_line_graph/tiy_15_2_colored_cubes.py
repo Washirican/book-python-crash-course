d6ebf3d76d4acacc67f96b270308547a752064c0
Washirican <danrod@uw.edu>
2020-01-29 22:15:44 -0800

    Chapter 15 try-it-yourself exercises.

---
+++ chapter_15/plotting_simple_line_graph/tiy_15_2_colored_cubes.py
@@ -0,0 +1,24 @@
+# --------------------------------------------------------------------------- #
+# D. Rodriguez, 2020-01-29
+# --------------------------------------------------------------------------- #
+import matplotlib.pyplot as plt
+
+x_values = range(-5000, 5000)
+y_values = [x**3 for x in x_values]
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=10, c=y_values, cmap=plt.cm.Greens)
+
+# Set chart title and label axes.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set the range for each axis.
+ax.axis([-150, 150, -1100000, 1100000])
+
+# Set size of tick labels.
+ax.tick_params(axis='both', which='major', labelsize=14)
+# plt.savefig('squares.png', bbox_inches='tight')
+plt.show()
