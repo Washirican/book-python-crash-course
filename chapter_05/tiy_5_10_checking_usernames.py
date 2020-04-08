29598ade36fd4fd285bfd5306953c770e4c06823
Washirican <danrod@uw.edu>
2020-04-07 19:07:02 -0700

    Chapter 5 Try It Yourself exercises

--- chapter_05/tiy_5_10_checking_usernames.py
+++ chapter_05/tiy_5_10_checking_usernames.py
@@ -3,4 +3,17 @@
 # Change Log: (Who, When, What)
 # D. Rodriguez, 2020-01-29, Initial release
 # --------------------------------------------------------------------------- #
-# TODO D. Rodriguez 2020-04-7: Finish this...
+
+current_users = ['washirican', 'Mastodonte', 'natifca', 'daniel_andres',
+                 'flaco_mcflaco', 'jordan']
+new_users = ['chilindrina', 'mastodonte', 'jirafales', 'florinda', 'chavo']
+
+current_users_lower = [user.lower() for user in current_users]
+
+for new_user in new_users:
+    if new_user.lower() in current_users_lower:
+        print('Username {} is already in use. Please, select a new Username.'
+              .format(new_user))
+    else:
+        print('Username {} is available!'.format(new_user))
+
--- chapter_05/tiy_5_11_ordinal_numbers.py
+++ chapter_05/tiy_5_11_ordinal_numbers.py
@@ -4,3 +4,15 @@
 # D. Rodriguez, 2020-01-29, Initial release
 # --------------------------------------------------------------------------- #
 # TODO D. Rodriguez 2020-04-7: Finish this...
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for number in numbers:
+    if number == 1:
+        print(str(number) + 'st')
+    elif number == 2:
+        print(str(number) + 'nd')
+    elif number == 3:
+        print(str(number) + 'rd')
+    else:
+        print(str(number) + 'th')
