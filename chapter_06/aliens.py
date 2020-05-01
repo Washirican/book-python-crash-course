0dff252bdd6e41345265d408dd73515d0817c332
Daniel Rodriguez <daniel_a_rodriguez@outlook.com>
2020-04-30 23:00:46 -0700

    Updates aliens.py exercise.

--- chapter_06/aliens.py
+++ chapter_06/aliens.py
@@ -1,18 +1,21 @@
-# Make an empty list for storing aliens.
 aliens = []
 
-# Make 30 green aliens.
 for alien_number in range(30):
     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
     aliens.append(new_alien)
+
+print(f'\nThere are {len(aliens)} total aliens in the list.')
 
 for alien in aliens[:3]:
     if alien['color'] == 'green':
         alien['color'] = 'yellow'
         alien['speed'] = 'medium'
         alien['points'] = 10
-    
-# Show the first 5 aliens.
+
+    elif alien['color'] == 'yellow':
+        alien['color'] = 'red'
+        alien['speed'] = 'fast'
+        alien['points'] = 15
+
 for alien in aliens[:5]:
     print(alien)
-print("...")
