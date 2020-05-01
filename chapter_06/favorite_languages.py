0f4bd3b036c066aa5fb8f07bee594186242f885a
Daniel Rodriguez <daniel_a_rodriguez@outlook.com>
2020-04-30 17:45:25 -0700

    Chapter 6 Try it Yourself exercises

--- chapter_06/favorite_languages.py
+++ chapter_06/favorite_languages.py
@@ -3,12 +3,28 @@ favorite_languages = {
     'sarah': 'c',
     'edward': 'ruby',
     'phil': 'python',
+    'albert': 'javascript'
     }
 
-friends = ['phil', 'sarah']
-for name in favorite_languages.keys():
-    print(name.title())
-    
-    if name in friends:
-        language = favorite_languages[name].title()
-        print(f"\t{name.title()}, I see you love {language}!")
+# for name in favorite_languages.keys():
+    # print(f'{name.title()}, thank you for taking the poll!')
+
+# print()
+
+# for name, language in favorite_languages.items():
+    # print(f'{name.title()}\'s favorite langiage is {language.title()}.')
+
+# friends = ['phil', 'sarah']
+
+# for name in favorite_languages.keys():
+#     print(f'Hi {name.title()}!')
+
+#     if name in friends:
+#         language = favorite_languages[name].title()
+#         print(f'\t{name.title()}, I see you love {language}!')
+
+# if 'erin' not in favorite_languages.keys():
+#     print(f'\nErin, please take our poll!')
+
+for name in sorted(favorite_languages.keys()):
+    print(f'{name.title()}, thank you for taking the poll!')
---
+++ chapter_06/tiy_6_1_person.py
@@ -0,0 +1,9 @@
+person = {
+    'first_name': 'michael', 
+    'last_name': 'jordan',
+    'age': 40,
+    'city': 'chicago',
+    }
+
+for key, value in person.items():
+    print(key, value
