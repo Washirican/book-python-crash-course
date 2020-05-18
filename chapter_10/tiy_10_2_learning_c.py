c7fe75caa7f8129fbdaac5d92fb940c09f8475d6
Daniel Rodriguez <daniel_a_rodriguez@outlook.com>
2020-05-18 09:10:54 -0700

    Try It Yourself exercises 1-2. Add clarification comments to TIY 10-2.

--- chapter_10/tiy_10_2_learning_c.py
+++ chapter_10/tiy_10_2_learning_c.py
@@ -3,7 +3,20 @@
 # --------------------------------------------------------------------------- #
 filename = 'learning_python.txt'
 
+print('\nRead entire file...')
 with open(filename) as file_object:
-    contents = file_object.read().replace('Python', 'C')
+    print(file_object.read().replace('Python', 'C'))
 
-    print(contents)
+
+print('\nRead one line at a time...')
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.replace('Python', 'C').strip())
+
+
+print('\nRead entire file into a list...')
+with open(filename) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.replace('Python', 'C').strip())
