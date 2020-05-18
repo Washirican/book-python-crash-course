bee5e9a05ebc6472c685bdfd87625ac0398696fe
Washirican <danrod@uw.edu>
2020-05-18 09:54:18 -0700

    Add requirements.txt file.

--- chapter_08/pizza.py
+++ chapter_08/pizza.py
@@ -1,5 +1,16 @@
-def make_pizza(size, *toppings):
-    """Summarize the pizza we are about to make."""
-    print(f"\nMaking a {size}-inch pizza with the following toppings:")
+# def make_pizza(size, *toppings):
+#     """Summarize the pizza we are about to make."""
+#     print(f"\nMaking a {size}-inch pizza with the following toppings:")
+#     for topping in toppings:
+#         print(f"- {topping}")
+
+
+def make_pizza(*toppings):
+    """Print list of toppings that have been requested."""
+    print(f'\nMaking a pizza with the following toppings:')
+
     for topping in toppings:
-        print(f"- {topping}")
+        print(f'\t{topping.title()}')
+
+
+make_pizza('pepperoni', 'olives', 'arugula', 'extra cheese')
