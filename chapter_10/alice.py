03cf5241e78788d497c21da868124f0c0c751632
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2020-05-21 15:06:05 -0700

    Chapter 10 examples and Try It Yourself exercise 6.

--- chapter_10/alice.py
+++ chapter_10/alice.py
@@ -1,12 +1,26 @@
+# ---- Original Code ----
+# filename = 'alice.txt'
+#
+# try:
+#     with open(filename, encoding='utf-8') as f:
+#         contents = f.read()
+# except FileNotFoundError:
+#     print(f"Sorry, the file {filename} does not exist.")
+# else:
+#     # Count the approximate number of words in the file.
+#     words = contents.split()
+#     num_words = len(words)
+#     print(f"The file {filename} has about {num_words} words.")
+
+# ---- My Code ----
 filename = 'alice.txt'
 
 try:
     with open(filename, encoding='utf-8') as f:
         contents = f.read()
 except FileNotFoundError:
-    print(f"Sorry, the file {filename} does not exist.")
+    print(f'Sorry, filename {filename} does not exist.')
 else:
-    # Count the approximate number of words in the file.
     words = contents.split()
     num_words = len(words)
-    print(f"The file {filename} has about {num_words} words.")
+    print(f'The title {filename} has about {num_words} words.')
--- chapter_10/word_count.py
+++ chapter_10/word_count.py
@@ -1,15 +1,42 @@
+# ---- Original Code ----
+# def count_words(filename):
+#     """Count the approximate number of words in a file."""
+#     try:
+#         with open(filename, encoding='utf-8') as f:
+#             contents = f.read()
+#     except FileNotFoundError:
+#         pass
+#     else:
+#         words = contents.split()
+#         num_words = len(words)
+#         print(f"The file {filename} has about {num_words} words.")
+#
+# filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
+# for filename in filenames:
+#     count_words(filename)
+
+
+# NOTE (D. Rodriguez 2020-05-21): My Code
 def count_words(filename):
     """Count the approximate number of words in a file."""
     try:
         with open(filename, encoding='utf-8') as f:
             contents = f.read()
     except FileNotFoundError:
-        pass
+        print(f'Sorry, file {filename} was not found.')
     else:
         words = contents.split()
         num_words = len(words)
-        print(f"The file {filename} has about {num_words} words.")
+        return num_words
 
+# ---- Using a single file ----
+# filename = 'alice.txt'
+# num_words = count_words(filename)
+# print(f'The file {filename} has about {num_words} words.')
+
+
+# NOTE (D. Rodriguez 2020-05-21):  Using multiple files
 filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
 for filename in filenames:
-    count_words(filename)
+    num_words = count_words(filename)
+    print(f'The file {filename} has about {num_words} words.')
