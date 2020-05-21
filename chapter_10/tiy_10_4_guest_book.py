d21bf9f3173c6f2750d62e4927b161fd9f8264b3
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2020-05-21 15:42:40 -0700

    Guest Book

--- chapter_10/tiy_10_4_guest_book.py
+++ chapter_10/tiy_10_4_guest_book.py
@@ -2,16 +2,3 @@
 # D. Rodriguez, 2020-05-18
 # --------------------------------------------------------------------------- #
 
-filename = 'guest_book.txt'
-
-active = True
-
-while active:
-    guest_name = input('\nEnter your name (\'q\' to quit): ')
-    if guest_name == 'q':
-        active = False
-    else:
-        with open(filename, 'a') as file_object:
-            print(f'Welcome back, {guest_name}!')
-            file_object.write(f'{guest_name}\n')
-
