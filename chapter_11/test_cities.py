6cc9c51da9390ec44dce7e07d30019d2765cc82b
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2020-05-28 15:52:52 -0700

    Chapter 11 examples and Try It Yourself exercises 1-2.

--- chapter_11/test_cities.py
+++ chapter_11/test_cities.py
@@ -3,8 +3,9 @@ from city_functions import city_country
 
 
 class CitiesTestCase(unittest.TestCase):
-    """Test that city, country and population are formatted correctly."""
+    """Test city, country functions."""
     def test_city_country_population(self):
+        """Test that city, country and population are formatted correctly."""
         formatted_string = city_country('bogota', 'colombia', '10,779,000')
         self.assertEqual(formatted_string, 'Bogota, Colombia - '
                                            'population 10,779,000')
---
+++ chapter_11/tiy_11_1_city_country.py
@@ -0,0 +1,9 @@
+# --------------------------------------------------------------------------- #
+# D. Rodriguez, 2020-05-28
+# --------------------------------------------------------------------------- #
+
+import unittest
+import test_cities
+
+suite = unittest.TestLoader().loadTestsFromModule(test_cities)
+unittest.TextTestRunner(verbosity=2).run(suite)
