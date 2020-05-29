dad4783489a24e9060091cd5c20aa62bcd995b75
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2020-05-28 17:12:46 -0700

    Chapter 11 examples and Try It Yourself exercises 3.

--- chapter_11/tiy_11_3_employee.py
+++ chapter_11/tiy_11_3_employee.py
@@ -2,3 +2,8 @@
 # D. Rodriguez, 2020-05-28
 # --------------------------------------------------------------------------- #
 
+import unittest
+import test_employee
+
+suite = unittest.TestLoader().loadTestsFromModule(test_employee)
+unittest.TextTestRunner(verbosity=2).run(suite)
