2beec3e78cc7d3a0fd7d5586b82a7cf5b6c9f028
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2020-10-18 22:25:43 -0700

    Make request to get new data from USGS API.

--- chapter_16/mapping_global_data_sets/eq_explore_data.py
+++ chapter_16/mapping_global_data_sets/eq_explore_data.py
@@ -1,10 +1,18 @@
 import json
+import requests
+
+request_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson'
+
+response = requests.get(request_url)
+
+new_data = response.json()
 
 # Explore the structure of the data.
-filename = 'data/eq_data_1_day_m1.json'
-with open(filename) as f:
-    all_eq_data = json.load(f)
+# filename = 'data/my_eq_data_1_day_m1.json'
+#
+# with open(filename, 'w') as f:
+#     json.dump(new_data, f)
 
 readable_file = 'data/readable_eq_data.json'
 with open(readable_file, 'w') as f:
-    json.dump(all_eq_data, f, indent=4)
+    json.dump(new_data, f, indent=4)
