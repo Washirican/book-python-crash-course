50ebe21a1045fc683fd18722c59517cefaaf7070
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2020-10-21 14:38:49 -0700

    Add marker customizations and additional data.

--- chapter_16/mapping_global_data_sets/chapter_examples.py
+++ chapter_16/mapping_global_data_sets/chapter_examples.py
@@ -7,7 +7,10 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 # Extra credit: Get recent earthquake data from USGS.
-request_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson'
+
+request_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/' \
+              '1.0_month.geojson'
+
 response = requests.get(request_url)
 recent_eq_data = response.json()
 
@@ -26,22 +29,46 @@ all_eq_dicts = recent_eq_data['features']
 # print(len(all_eq_dicts))
 
 # Get earthquake magnitudes and store them in a list.
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
 
+    # Get additional data from file for hover text.
+    title = eq_dict['properties']['title']
+
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
-print(mags[:10])
-print(lons[:10])
-print(lats[:10])
+# print(mags[:10])
+# print(lons[:10])
+# print(lats[:10])
 
 # Map the earthquakes.
-data = [Scattergeo(lon=lons, lat=lats)]
+# data = [Scattergeo(lon=lons, lat=lats)]
+
+# Define chart in a way that is better to customize it.
+data = [
+    {
+        'type': 'scattergeo',
+        'lon': lons,
+        'lat': lats,
+        'text': hover_texts,
+        'marker': {
+            # Define marker size depending on earthquake magnitude.
+            'size': [5*mag for mag in mags],
+            # Customizing marker colors.
+            'color': mags,
+            'colorscale': 'Viridis',
+            'reversescale': True,
+            'colorbar': {'title': 'Magnitude'},
+            }
+        }
+    ]
+
 my_layout = Layout(title='Global Earthquakes')
 
 fig = {'data': data, 'layout': my_layout}
