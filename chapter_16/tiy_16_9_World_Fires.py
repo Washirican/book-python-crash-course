c741c4e6c23da6f01202b0dacb756c67d9a151f4
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2020-10-29 09:07:03 -0700

    Try it Yourself exercise 9: Revise map properties to show country borders and zoom in to data location.

--- chapter_16/tiy_16_9_World_Fires.py
+++ chapter_16/tiy_16_9_World_Fires.py
@@ -2,37 +2,63 @@
 # D. Rodriguez, 2020-10-27, File Created.
 # --------------------------------------------------------------------------- #
 import csv
+from datetime import datetime
+
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 filename = 'mapping_global_data_sets/data/modis_2019_United_States.csv'
-latitudes, longitudes, brightnesses = [], [], []
+latitudes, longitudes = [], []
+brightnesses, dates = [], []
+hover_texts = []
+
+num_rows = 10_000
 
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
+    # Get latitudes, longitudes, brightness and dates.
+    row_num = 0
     for row in reader:
+        dates.append(datetime.strptime(row[5], '%Y-%m-%d'))
+        brightnesses.append(float(row[2]))
+        hover_texts.append(
+                f"{datetime.strptime(row[5], '%Y-%m-%d').strftime('%m/%d/%y')} "
+                f"- {float(row[2])}")
+
         latitudes.append(float(row[0]))
         longitudes.append(float(row[1]))
-        brightnesses.append(float(row[2]))
+
+        row_num += 1
+        if row_num == num_rows:
+            break
 
 data = [
     {
         'type': 'scattergeo',
         'lon': longitudes,
         'lat': latitudes,
+        'text': hover_texts,
         'marker': {
-            'size': [brightness/100 for brightness in brightnesses],
+            'size': [brightness/20 for brightness in brightnesses],
             'color': brightnesses,
-            'colorscale': 'Viridis',
-            'reversescale': True,
+            'colorscale': 'YlOrRd',
+            # 'reversescale': True,
             'colorbar': {'title': 'Brightness'},
             }
         }
     ]
 
-my_layout = Layout(title='Wildfires')
+my_layout = Layout(title='United States Wildfires')
+my_layout.geo.update(
+        showcountries=True,
+        fitbounds='locations',
+        )
 
-fig = {'data': data, 'layout': my_layout}
+fig = {
+    'data': data,
+    'layout': my_layout,
+    }
+
 offline.plot(fig, filename='us_wildfires.html')
